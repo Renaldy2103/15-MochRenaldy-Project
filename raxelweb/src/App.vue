1a1320dff8a78b910ad15849357d9a2bb09a51a6
Renaldy2103 <renaldy.syaputra2103@gmail.com>
2022-03-22 12:41:50 +0700

    Membuat Header

--- raxelweb/src/App.vue
+++ raxelweb/src/App.vue
@@ -1,60 +1,104 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+  <b-container fluid="md">
+    <div class="navbar">
+      <b-nav>
+        <h3>
+          <b
+            >RAXEL
+            <div>WEB</div></b
+          >
+        </h3>
+        <b-nav-item active>Home</b-nav-item>
+        <b-nav-item>Our Service</b-nav-item>
+        <b-nav-item>Our Client</b-nav-item>
+        <b-nav-item>Team</b-nav-item>
+        <b-nav-item>Contact Us</b-nav-item>
+      </b-nav>
+      <div class="btn-call-us-now">
+        <b-button variant="btn-call-us-now outline-primary"
+          >Call Us Now</b-button
+        >
+      </div>
+      <div class="gambarpojok">
+        <img
+          class="logo"
+          style="width: 700px; height: 650px"
+          alt="Gambar Pojok"
+          src="./assets/Gambar-Pojok.png"
         />
       </div>
+    </div>
+    <div>
+      <b-jumbotron>
+        <template #header>Hai, Welcome</template>
 
-      <v-spacer></v-spacer>
+        <p>Selamat Datang di RAXEL WEB, This is my Portofolio Web :)</p>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+        <b-button variant="primary" class="btn-start" href="#"
+          >Start Your Project</b-button
+        >
+        <b-button variant="success" class="btn-learn" href="#"
+          >Learn More</b-button
+        >
+      </b-jumbotron>
+    </div>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
 </script>
+
+<style>
+.nav-item {
+  font-size: 14px;
+  margin-left: 40px;
+  margin-top: 20px;
+  color: #000;
+}
+.btn-call-us-now {
+  background-color: white;
+  z-index: 1;
+  border-radius: 5px;
+  padding-right: 35px;
+  padding-left: 35px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.gambarpojok {
+  position: absolute;
+  left: 750px;
+  right: -0.1%;
+  top: -50px;
+  bottom: -0px;
+  transform: rotate(-20.43deg);
+  opacity: 40%;
+  background-color: E0C4FC;
+}
+
+.jumbotron {
+  font-family: Nunito sans-serif;
+  margin-top: 10px;
+  background-color: white;
+  margin-top: 100px;
+}
+.btn-start {
+  background-color: #744fc6;
+  font-family: sans-serif;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  padding-right: 30px;
+  padding-left: 30px;
+  border-radius: 12px;
+}
+.btn-learn {
+  background-color: #744fc6;
+  font-family: sans-serif;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  padding-right: 30px;
+  padding-left: 30px;
+  border-radius: 12px;
+  margin-left: 10px;
+}
+</style>
