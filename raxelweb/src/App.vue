8bb6bfd525ba2bb1f64310538b31d57a788318bd
Renaldy2103 <renaldy.syaputra2103@gmail.com>
2022-03-23 14:51:34 +0700

    Proses Membuat body

--- raxelweb/src/App.vue
+++ raxelweb/src/App.vue
@@ -31,16 +31,64 @@
     <div>
       <b-jumbotron>
         <template #header>Hai, Welcome</template>
+        <p>Selamat Datang di RAXEL WEB</p>
+        <p>This is my Portofolio Web :)</p>
 
-        <p>Selamat Datang di RAXEL WEB, This is my Portofolio Web :)</p>
-
-        <b-button variant="primary" class="btn-start" href="#"
+        <b-button variant="primary" class="btn-start btn-lg"
           >Start Your Project</b-button
         >
-        <b-button variant="success" class="btn-learn" href="#"
+        <b-button variant="light" class="btn-learn btn-outline-dark btn-lg"
           >Learn More</b-button
         >
       </b-jumbotron>
+    </div>
+    <div class="our-service text-center">
+      <p class="lead"></p>
+      <h1>Our services</h1>
+      <p>
+        We become part of your team, meeting regularly to plan content,
+        facilitate business goals and get creative as a group.
+      </p>
+
+      <div class="rectangle1">
+        <img
+          src="./assets/Rectangle 2.png"
+          style="width: 600px; height: 600px"
+        />
+      </div>
+      <v-card class="mx-auto" max-width="400">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+        </v-img>
+
+        <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+        <v-card-text class="text--primary">
+          <div>Whitehaven Beach</div>
+
+          <div>Whitsunday Island, Whitsunday Islands</div>
+        </v-card-text>
+      </v-card>
+
+      <v-card class="mx-auto" max-width="400">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+        </v-img>
+
+        <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+
+        <v-card-text class="text--primary">
+          <div>Whitehaven Beach</div>
+
+          <div>Whitsunday Island, Whitsunday Islands</div>
+        </v-card-text>
+      </v-card>
     </div>
   </b-container>
 </template>
@@ -50,7 +98,7 @@
 
 <style>
 .nav-item {
-  font-size: 14px;
+  font-size: 16px;
   margin-left: 40px;
   margin-top: 20px;
   color: #000;
@@ -69,7 +117,7 @@
   position: absolute;
   left: 750px;
   right: -0.1%;
-  top: -50px;
+  top: -150px;
   bottom: -0px;
   transform: rotate(-20.43deg);
   opacity: 40%;
@@ -82,23 +130,35 @@
   background-color: white;
   margin-top: 100px;
 }
+
 .btn-start {
   background-color: #744fc6;
   font-family: sans-serif;
-  padding-bottom: 20px;
-  padding-top: 20px;
-  padding-right: 30px;
-  padding-left: 30px;
   border-radius: 12px;
+  margin-top: 30px;
 }
 .btn-learn {
-  background-color: #744fc6;
+  border: 1px solid #744fc6;
   font-family: sans-serif;
-  padding-bottom: 20px;
-  padding-top: 20px;
-  padding-right: 30px;
-  padding-left: 30px;
   border-radius: 12px;
-  margin-left: 10px;
+  margin-left: 20px;
+  margin-top: 30px;
+}
+.our-service {
+  background-color: #f5f5f5;
+  margin-top: 150px;
+  border-radius: 16px;
+  padding-bottom: 40px;
+}
+.lead {
+  font-family: sans-serif;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.rectangle1 {
+  margin-left: -400px;
+  position: absolute;
+  margin-top: -180px;
+  transform: rotate(-70deg);
 }
 </style>
